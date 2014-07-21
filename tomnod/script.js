a107997f8d86102905cb478f92b0a6a9aad2b706
Keiichi Lindley <keiichi.lindley@gmail.com>
2014-07-21 15:46:19 -0600

    Fixed Class URL

--- tomnod/script.js
+++ tomnod/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   console.log('Initiated!');
-  OpenBadges.issue(['http://www.example.com', '127.0.0.1'], function(errors, successes) { 
+  OpenBadges.issue(['http://digininjadev.github.io/tomnod/1.json'], function(errors, successes) { 
   	console.log(errors);
+  	console.log(successes);
   });
 });
